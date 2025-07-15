7f53e1ca794fa62b5b8c1df7e462cf6d0ac8c81b
JeffHC0911 <jefferhc0911@gmail.com>
2025-07-14 23:16:41 -0500

    Update BD Schema

--- modules/dynamodb/main.tf
+++ modules/dynamodb/main.tf
@@ -1,18 +1,33 @@
-resource "aws_dynamodb_table" "surveys" {
+resource "aws_dynamodb_table" "surveys_table" {
   name         = var.surveys_table_name
   billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "survey_id"
+  hash_key     = "PK"
+  range_key    = "SK"
+
+  attribute {
+    name = "PK"
+    type = "S"
+  }
+
+  attribute {
+    name = "SK"
+    type = "S"
+  }
+
+  attribute {
+    name = "entityType"
+    type = "S"
+  }
+
+  attribute {
+    name = "admin_id"
+    type = "S"
+  }
 
   attribute {
     name = "survey_id"
     type = "S"
   }
-}
-
-resource "aws_dynamodb_table" "survey_versions" {
-  name         = var.survey_versions_table_name
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "version_id"
 
   attribute {
     name = "version_id"
@@ -20,123 +35,57 @@ resource "aws_dynamodb_table" "survey_versions" {
   }
 
   attribute {
-    name = "survey_id"
+    name = "respondent_email"
+    type = "S"
+  }
+
+  attribute {
+    name = "created_at"
+    type = "S"
+  }
+
+  attribute {
+    name = "submitted_at"
     type = "S"
   }
 
   global_secondary_index {
-    name            = "survey_id-index"
+    name            = "GSI1"
+    hash_key        = "entityType"
+    range_key       = "created_at"
+    projection_type = "ALL"
+  }
+
+  global_secondary_index {
+    name            = "GSI2"
+    hash_key        = "admin_id"
+    range_key       = "PK"
+    projection_type = "ALL"
+  }
+
+  global_secondary_index {
+    name            = "GSI3"
     hash_key        = "survey_id"
+    range_key       = "SK"
     projection_type = "ALL"
-  }
-}
-
-resource "aws_dynamodb_table" "questions" {
-  name         = var.questions_table_name
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "question_id"
-
-  attribute {
-    name = "question_id"
-    type = "S"
-  }
-
-  attribute {
-    name = "version_id"
-    type = "S"
   }
 
   global_secondary_index {
-    name            = "version_id-index"
+    name            = "GSI4"
     hash_key        = "version_id"
+    range_key       = "SK"
     projection_type = "ALL"
-  }
-}
-
-resource "aws_dynamodb_table" "responses" {
-  name         = var.responses_table_name
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "response_id"
-
-  attribute {
-    name = "response_id"
-    type = "S"
-  }
-
-  attribute {
-    name = "version_id"
-    type = "S"
   }
 
   global_secondary_index {
-    name            = "version_id-index"
-    hash_key        = "version_id"
+    name            = "GSI5"
+    hash_key        = "respondent_email"
+    range_key       = "submitted_at"
     projection_type = "ALL"
   }
-}
 
-resource "aws_dynamodb_table" "answers" {
-  name         = var.answers_table_name
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "answer_id"
-
-  attribute {
-    name = "answer_id"
-    type = "S"
-  }
-
-  attribute {
-    name = "response_id"
-    type = "S"
-  }
-
-  global_secondary_index {
-    name            = "response_id-index"
-    hash_key        = "response_id"
-    projection_type = "ALL"
-  }
-}
-
-resource "aws_dynamodb_table" "files" {
-  name         = var.files_table_name
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "file_id"
-
-  attribute {
-    name = "file_id"
-    type = "S"
-  }
-}
-
-resource "aws_dynamodb_table" "templates" {
-  name         = var.templates_table_name
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "template_id"
-
-  attribute {
-    name = "template_id"
-    type = "S"
-  }
-}
-
-resource "aws_dynamodb_table" "template_questions" {
-  name         = var.template_questions_table_name
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "template_question_id"
-
-  attribute {
-    name = "template_question_id"
-    type = "S"
-  }
-
-  attribute {
-    name = "template_id"
-    type = "S"
-  }
-
-  global_secondary_index {
-    name            = "template_id-index"
-    hash_key        = "template_id"
-    projection_type = "ALL"
+  tags = {
+    Environment = "dev"
+    Project     = "IntelligentSurveySystem"
   }
 }
--- modules/dynamodb/outputs.tf
+++ modules/dynamodb/outputs.tf
@@ -1,64 +1,7 @@
 output "surveys_table_name" {
-  value = aws_dynamodb_table.surveys.name
-}
-
-output "survey_versions_table_name" {
-  value = aws_dynamodb_table.survey_versions.name
-}
-
-output "questions_table_name" {
-  value = aws_dynamodb_table.questions.name
-}
-
-output "responses_table_name" {
-  value = aws_dynamodb_table.responses.name
-}
-
-output "answers_table_name" {
-  value = aws_dynamodb_table.answers.name
-}
-
-output "files_table_name" {
-  value = aws_dynamodb_table.files.name
-}
-
-output "templates_table_name" {
-  value = aws_dynamodb_table.templates.name
-}
-
-output "template_questions_table_name" {
-  value = aws_dynamodb_table.template_questions.name
+  value = aws_dynamodb_table.surveys_table.name
 }
 
 output "surveys_table_arn" {
-  value = aws_dynamodb_table.surveys.arn
+  value = aws_dynamodb_table.surveys_table.arn
 }
-
-output "survey_versions_table_arn" {
-  value = aws_dynamodb_table.survey_versions.arn
-}
-
-output "questions_table_arn" {
-  value = aws_dynamodb_table.questions.arn
-}
-
-output "responses_table_arn" {
-  value = aws_dynamodb_table.responses.arn
-}
-
-output "answers_table_arn" {
-  value = aws_dynamodb_table.answers.arn
-}
-
-output "files_table_arn" {
-  value = aws_dynamodb_table.files.arn
-}
-
-output "templates_table_arn" {
-  value = aws_dynamodb_table.templates.arn
-}
-
-output "template_questions_table_arn" {
-  value = aws_dynamodb_table.template_questions.arn
-}
-
